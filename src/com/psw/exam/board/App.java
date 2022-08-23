942cc02b8c6c2018f71966295365a83adc827b43
SangWon7242 <gotkdqja@gmail.com>
2022-08-23 20:50:28 +0900

    게시물 리스팅, 추가 기능 구현

--- src/com/psw/exam/board/App.java
+++ src/com/psw/exam/board/App.java
@@ -1,17 +1,49 @@
 package com.psw.exam.board;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
   public void run() {
     Scanner sc = Container.scanner;
+    List<Article> articles = new ArrayList<>();
+    int articleLastId = 0;
 
     while (true) {
       System.out.printf("명령어) ");
       String cmd = sc.nextLine().trim();
+      if(cmd.equals("/usr/article/write")) {
+        System.out.println("== 게시물 등록 ==");
+        System.out.printf("제목 : ");
+        String title = sc.nextLine();
+        System.out.printf("내용 : ");
+        String body = sc.nextLine();
 
-      if(cmd.equals("/usr/article/list")) {
+        int id = ++articleLastId;
+
+        Article article = new Article(id, title, body);
+
+        articles.add(article);
+
+        System.out.println("생성된 게시물 객체 : " + article);
+        System.out.printf("%d번 게시물이 등록 되었습니다.\n", article.id);
+
+      }
+     else if(cmd.equals("/usr/article/list")) {
         System.out.println("== 게시물 리스트 ==");
+
+        if( articles.size() == 0 ) {
+          System.out.println("게시물이 존재하지 않습니다.");
+          continue;
+        }
+
+        System.out.println("번호 / 제목");
+
+        for( Article article : articles ) {
+          System.out.printf("%d / %s\n", article.id, article.title);
+        }
+
       }
       else if(cmd.equals("exit")) {
         System.out.println("== 시스템 종료 ==");
---
+++ src/com/psw/exam/board/Article.java
@@ -0,0 +1,18 @@
+package com.psw.exam.board;
+
+public class Article {
+  int id;
+  String title;
+  String body;
+
+  Article(int id, String title, String body) {
+    this.id = id;
+    this.title = title;
+    this.body = body;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("{id : %d, title : \"%s\", body : \"%s\"}", id, title, body);
+  }
+}
