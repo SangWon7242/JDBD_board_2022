942bd0fa0079e12aef3daaa91213d888d0171894
SangWon7242 <gotkdqja@gmail.com>
2022-08-25 20:23:39 +0900

    JDBC SELECT 테스트

--- src/com/psw/exam/board/Article.java
+++ src/com/psw/exam/board/Article.java
@@ -2,10 +2,21 @@ package com.psw.exam.board;
 
 public class Article {
   int id;
+  String regDate;
+  String updateDate;
   String title;
   String body;
 
-  Article(int id, String title, String body) {
+  public Article(int id, String regDate, String updateDate, String title, String body) {
+    this.id = id;
+    this.regDate = regDate;
+    this.updateDate = updateDate;
+    this.title = title;
+    this.body = body;
+  }
+
+
+  public Article(int id, String title, String body) {
     this.id = id;
     this.title = title;
     this.body = body;
@@ -13,6 +24,7 @@ public class Article {
 
   @Override
   public String toString() {
-    return String.format("{id : %d, title : \"%s\", body : \"%s\"}", id, title, body);
+    return String.format("{id : %d, regDate : %s, updateDate : %s, title : \"%s\", body : \"%s\"}",
+        id, regDate, updateDate, title, body);
   }
 }
