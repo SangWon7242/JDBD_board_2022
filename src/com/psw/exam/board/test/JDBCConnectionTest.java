5dbefccfd9ba36e4f23eba032cc10237f5bf4cce
SangWon7242 <gotkdqja@gmail.com>
2022-08-25 19:40:32 +0900

    JDBC 인서트 테스트

--- src/com/psw/exam/board/test/JDBCConnectionTest.java
+++ src/com/psw/exam/board/test/JDBCConnectionTest.java
@@ -2,34 +2,31 @@ package com.psw.exam.board.test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class JDBCTest {
+public class JDBCConnectionTest {
   public static void main(String[] args) {
 
     Connection conn = null;
 
-    try{
+    try {
       Class.forName("com.mysql.jdbc.Driver");
 
       String url = "jdbc:mysql://127.0.0.1:3306/text_board?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
 
       conn = DriverManager.getConnection(url, "sbsst", "sbs123414");
-
-    }
-    catch(ClassNotFoundException e){
+      System.out.println("연결 성공");
+    } catch (ClassNotFoundException e) {
       System.out.println("드라이버 로딩 실패");
-    }
-    catch(SQLException e){
+    } catch (SQLException e) {
       System.out.println("에러: " + e);
-    }
-    finally{
-      try{
-        if( conn != null && !conn.isClosed()){
+    } finally {
+      try {
+        if (conn != null && !conn.isClosed()) {
           conn.close();
         }
-      }
-      catch( SQLException e){
+      } catch (SQLException e) {
         e.printStackTrace();
       }
     }
