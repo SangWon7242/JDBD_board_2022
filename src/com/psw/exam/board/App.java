b3970c329702f225b36729cbb36a9b76391755c1
SangWon7242 <gotkdqja@gmail.com>
2022-08-25 20:07:02 +0900

    게시물 작성시 DB 저장

--- src/com/psw/exam/board/App.java
+++ src/com/psw/exam/board/App.java
@@ -1,5 +1,9 @@
 package com.psw.exam.board;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -21,6 +25,46 @@ public class App {
         String body = sc.nextLine();
 
         int id = ++articleLastId;
+
+        Connection conn = null;
+        PreparedStatement pstat = null;
+
+        try {
+          Class.forName("com.mysql.jdbc.Driver");
+
+          String url = "jdbc:mysql://127.0.0.1:3306/text_board?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+          conn = DriverManager.getConnection(url, "sbsst", "sbs123414");
+
+          String sql = "INSERT INTO article";
+          sql += " SET regDate = NOW()";
+          sql += ", updateDate = NOW()";
+          sql += ", title = \"" + title + "\"";
+          sql += ", `body` = \"" + body + "\";";
+
+          pstat = conn.prepareStatement(sql);
+          pstat.executeUpdate();
+
+        } catch (ClassNotFoundException e) {
+          System.out.println("드라이버 로딩 실패");
+        } catch (SQLException e) {
+          System.out.println("에러: " + e);
+        } finally {
+          try {
+            if (conn != null && !conn.isClosed()) {
+              conn.close();
+            }
+          } catch (SQLException e) {
+            e.printStackTrace();
+          }
+          try {
+            if (pstat != null && !pstat.isClosed()) {
+              pstat.close();
+            }
+          } catch (SQLException e) {
+            e.printStackTrace();
+          }
+        }
 
         Article article = new Article(id, title, body);
 
